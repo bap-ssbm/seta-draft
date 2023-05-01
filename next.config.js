84d57c58aaa33ff3a6d6e31fad23a2452b8cc506
Kentaro <94794692+bap-ssbm@users.noreply.github.com>
2023-05-01 16:52:54 +0900

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -9,6 +9,14 @@ const nextConfig = {
   },
   images: {
     domains: ['images.pexels.com'],
+  },
+  basePath: '/[lng]',
+  exportPathMap: function(defaultPathMap) {
+    return {
+      '/en/page': { page: '/page', query: { lng: 'en' } },
+      '/ja/page': { page: '/page', query: { lng: 'ja' } },
+      ...defaultPathMap
+    }
   }
 }
 
