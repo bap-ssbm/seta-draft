3ef57e2123c952a32b4c60e4d08de78cf59d018f
Kentaro <94794692+bap-ssbm@users.noreply.github.com>
2023-05-01 17:03:33 +0900

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -9,23 +9,7 @@ const nextConfig = {
   },
   images: {
     domains: ['images.pexels.com'],
-  },
-
-  async exportPathMap(defaultPathMap) {
-    return Object.keys(defaultPathMap).reduce((pathMap, path) => {
-      const localizedPaths = ['/en', '/ja'].map((locale) => ({
-        params: { lng: locale.slice(1) },
-        query: { __nextLocale: locale.slice(1), __nextDefaultLocale: 'ja' },
-        locale,
-        as: `${locale}${path}`,
-      }));
-
-      return {
-        ...pathMap,
-        [path]: { page: path, ...localizedPaths.reduce((_, path) => ({ ..._, [path.locale]: path }), {}) },
-      };
-    }, {});
-  },
+  }
 }
 
 module.exports = nextConfig
